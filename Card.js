834573a8a7e2bc853e77070a31260413b472586c
Nicklas Hansson <nicke_h90@hotmail.com>
2022-07-19 11:08:38 +0200

    Setting up the beginning of the card class

---
+++ Card.js
@@ -0,0 +1,18 @@
+class Card {
+  // Initializing the card object with its params
+  constructor(face, suit, value) {
+    this.face = face
+    this.suit = suit
+    this.value = value
+  }
+  
+  // return a "visual" representation of the card
+  getCard() {
+    return [this.face, this.suit]
+  }
+
+  // return the actual value of the card
+  getValue() {
+    return this.value
+  }
+}
